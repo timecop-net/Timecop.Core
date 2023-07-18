6556934c88becf9e054ca885e20235c413c5e9d3
Dmytro Khmara <dmytro@dmytrokhmara.com>
2023-07-18 21:57:29 +0100

    refactor: remove TimecopBase - composition FTW! (#5)

--- src/Timecop.Core/Context/TimecopContextStore.cs
+++ src/Timecop.Core/Context/TimecopContextStore.cs
@@ -3,28 +3,32 @@ using TCop.Core.Time;
 
 namespace TCop.Core.Context;
 
+public delegate void MutateContextWithCurrentPointInTime(ref TimecopContext context, PointInTime utcNow);
+public delegate void MutateContext(ref TimecopContext context);
+
 public class TimecopContextStore
 {
     private static readonly AsyncLocal<TimecopContext?> AsyncContext = new();
 
-    public delegate void MutateContextWithCurrentDateTime(ref TimecopContext context, PointInTime utcNow);
-    public delegate void MutateContext(ref TimecopContext context);
-
-    public void Mutate(MutateContextWithCurrentDateTime mutate)
+    public PointInTime Mutate(MutateContextWithCurrentPointInTime mutate)
     {
         var asyncContext = AsyncContext.Value ?? new TimecopContext();
 
-        mutate(ref asyncContext, PointInTime.Now);
+        var realNow = PointInTime.Now;
+
+        mutate(ref asyncContext, realNow);
 
         AsyncContext.Value = asyncContext;
+
+        return asyncContext.GetNow(realNow);
     }
 
-    public void Mutate(MutateContext mutate)
+    public PointInTime Mutate(MutateContext mutate)
     {
-        Mutate((ref TimecopContext context, PointInTime _) => mutate(ref context));
+        return Mutate((ref TimecopContext context, PointInTime _) => mutate(ref context));
     }
 
-    public static PointInTime AsyncContextUtcNow => AsyncContext.Value?.GetUtcNow(PointInTime.Now) ?? PointInTime.Now;
+    public static PointInTime AsyncContextUtcNow => AsyncContext.Value?.GetNow(PointInTime.Now) ?? PointInTime.Now;
 
     public void ResetContext()
     {
---
+++ test/Timecop.Core.Tests/TimecopContextStoreTests.cs
@@ -0,0 +1,91 @@
+﻿using FluentAssertions;
+using TCop.Core.Context;
+using TCop.Core.Time;
+
+namespace TCop.Core.Tests;
+
+public class TimecopContextStoreTests
+{
+    private static readonly TimeSpan DateTimeComparisonPrecision = TimeSpan.FromMilliseconds(50);
+
+    [Fact]
+    public void AsyncContextUtcNow_NoContextStoreInstanceCreated_ShouldReturnCurrentTime()
+    {
+        TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow, DateTimeComparisonPrecision);
+    }
+
+    [Fact]
+    public void AsyncContextUtcNow_ContextStoreInstanceCreated_ShouldReturnCurrentTime()
+    {
+        var _ = new TimecopContextStore();
+        TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow, DateTimeComparisonPrecision);
+    }
+
+    [Fact]
+    public async Task AsyncContextUtcNow_TwoNestedStores_ChildInheritsContext_ButParentDoesntSeeChildChanges()
+    {
+        var store1 = new TimecopContextStore();
+
+        var baseFrozen = store1.Mutate((ref TimecopContext o, PointInTime now) => o.Freeze(now));
+        
+        store1.Mutate((ref TimecopContext o) => o.TravelBy(TimeSpan.FromMinutes(10)));
+
+        await Task.Run(async () =>
+        {
+            var store2 = new TimecopContextStore();
+
+            store2.Mutate((ref TimecopContext o) => o.TravelBy(TimeSpan.FromMinutes(20)));
+            store2.Mutate((ref TimecopContext o, PointInTime now) => o.Resume(now));
+
+            await Task.Delay(500);
+
+            TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(
+                baseFrozen.DateTimeUtc
+                    .AddMinutes(30)
+                    .AddMilliseconds(500),
+                DateTimeComparisonPrecision);
+        });
+
+        TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(
+            baseFrozen.DateTimeUtc
+                .AddMinutes(10),
+            DateTimeComparisonPrecision);
+    }
+
+
+    [Fact]
+    public void AsyncContextUtcNow_TwoParallelStores_RunInTwoThreadsInParallel_DoNotInterfereWithEachOther()
+    {
+        // 0 sec: T1 goes forward 10 minutes, asserts time, waits 2 secs
+        // 1 sec: T2 asserts time, goes forward 10 minutes
+        // 2 sec: T1 asserts time is still 10 minutes forward
+
+        var task1 = Task.Run(() =>
+        {
+            var store = new TimecopContextStore();
+
+            store.Mutate((ref TimecopContext o) => o.TravelBy(TimeSpan.FromMinutes(10)));
+
+            TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow.AddMinutes(10), DateTimeComparisonPrecision);
+
+            Thread.Sleep(2000);
+
+            TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow.AddMinutes(10), DateTimeComparisonPrecision);
+        });
+
+        var task2 = Task.Run(() =>
+        {
+            var store = new TimecopContextStore();
+
+            Thread.Sleep(2000);
+
+            TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow, DateTimeComparisonPrecision);
+
+            store.Mutate((ref TimecopContext o) => o.TravelBy(TimeSpan.FromMinutes(10)));
+        });
+
+        Task.WaitAll(task1, task2);
+
+        TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow, DateTimeComparisonPrecision);
+    }
+}
