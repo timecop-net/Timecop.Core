2cde10694c3853e26e868bfe48a88ad8137065b4
Dmytro Khmara <dmytro@dmytrokhmara.com>
2023-07-30 20:44:41 +0100

    feat: add an instance context and expose it via InstanceContextNow. (#10)

--- src/Timecop.Core/Context/TimecopContextStore.cs
+++ src/Timecop.Core/Context/TimecopContextStore.cs
@@ -8,16 +8,20 @@ public delegate void MutateContext(ref TimecopContext context);
 public class TimecopContextStore
 {
     private static readonly AsyncLocal<TimecopContext?> AsyncContext = new();
+    private TimecopContext? _instanceContext;
 
     public PointInTime Mutate(MutateContextWithCurrentPointInTime mutate)
     {
         var asyncContext = AsyncContext.Value ?? new TimecopContext();
+        var instanceContext = _instanceContext ?? new TimecopContext();
 
         var realNow = PointInTime.Now;
 
         mutate(ref asyncContext, realNow);
+        mutate(ref instanceContext, realNow);
 
         AsyncContext.Value = asyncContext;
+        _instanceContext = instanceContext;
 
         return asyncContext.GetNow(realNow);
     }
@@ -27,7 +31,8 @@ public class TimecopContextStore
         return Mutate((ref TimecopContext context, PointInTime _) => mutate(ref context));
     }
 
-    public static PointInTime AsyncContextUtcNow => AsyncContext.Value?.GetNow(PointInTime.Now) ?? PointInTime.Now;
+    public static PointInTime AsyncContextNow => AsyncContext.Value?.GetNow(PointInTime.Now) ?? PointInTime.Now;
+    public PointInTime InstanceContextNow => _instanceContext?.GetNow(PointInTime.Now) ?? PointInTime.Now;
 
     public void ResetContext()
     {
