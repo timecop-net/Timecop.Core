894c37e40604f7570dd277b970a5b3b12f0c31e0
Dmytro Khmara <dmytro@dmytrokhmara.com>
2023-07-30 20:43:05 +0100

    feat: add an instance context and expose it via InstanceContextNow.

--- test/Timecop.Core.Tests/TimecopContextStoreTests.cs
+++ test/Timecop.Core.Tests/TimecopContextStoreTests.cs
@@ -8,20 +8,27 @@ public class TimecopContextStoreTests
     private static readonly TimeSpan DateTimeComparisonPrecision = TimeSpan.FromMilliseconds(50);
 
     [Fact]
-    public void AsyncContextUtcNow_NoContextStoreInstanceCreated_ShouldReturnCurrentTime()
+    public void AsyncContextNow_NoContextStoreInstanceCreated_ShouldReturnCurrentTime()
     {
-        TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow, DateTimeComparisonPrecision);
+        TimecopContextStore.AsyncContextNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow, DateTimeComparisonPrecision);
     }
 
     [Fact]
-    public void AsyncContextUtcNow_ContextStoreInstanceCreated_ShouldReturnCurrentTime()
+    public void AsyncContextNow_ContextStoreInstanceCreated_ShouldReturnCurrentTime()
     {
         var _ = new TimecopContextStore();
-        TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow, DateTimeComparisonPrecision);
+        TimecopContextStore.AsyncContextNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow, DateTimeComparisonPrecision);
     }
 
     [Fact]
-    public async Task AsyncContextUtcNow_TwoNestedStores_ChildInheritsContext_ButParentDoesntSeeChildChanges()
+    public void InstanceContextNow_ByDefault_ShouldReturnCurrentTime()
+    {
+        var store = new TimecopContextStore();
+        store.InstanceContextNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow, DateTimeComparisonPrecision);
+    }
+
+    [Fact]
+    public async Task AsyncContextNow_TwoNestedStores_ChildInheritsContext_ButParentDoesntSeeChildChanges()
     {
         var store1 = new TimecopContextStore();
 
@@ -38,22 +45,41 @@ public class TimecopContextStoreTests
 
             await Task.Delay(500);
 
-            TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(
+            TimecopContextStore.AsyncContextNow.DateTimeUtc.Should().BeCloseTo(
                 baseFrozen.DateTimeUtc
                     .AddMinutes(30)
                     .AddMilliseconds(500),
                 DateTimeComparisonPrecision);
         });
 
-        TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(
+        TimecopContextStore.AsyncContextNow.DateTimeUtc.Should().BeCloseTo(
             baseFrozen.DateTimeUtc
                 .AddMinutes(10),
             DateTimeComparisonPrecision);
     }
 
+    [Fact]
+    public void InstanceContextNow_OwnedByTwoSeparateStores_ShouldActIndependently()
+    {
+        var store1 = new TimecopContextStore();
+        var store2 = new TimecopContextStore();
+
+        var freeze1 = DateTime.UtcNow.AddDays(1);
+        var freeze2 = DateTime.UtcNow.AddDays(2);
+
+        store1.Mutate((ref TimecopContext context, PointInTime now) =>
+            context.FreezeAt(PointInTime.FromBclTicks(freeze1.Ticks), now));
+
+        store2.Mutate((ref TimecopContext context, PointInTime now) =>
+            context.FreezeAt(PointInTime.FromBclTicks(freeze2.Ticks), now));
+
+        store1.InstanceContextNow.DateTimeUtc.Should().Be(freeze1);
+        store2.InstanceContextNow.DateTimeUtc.Should().Be(freeze2);
+    }
+
 
     [Fact]
-    public void AsyncContextUtcNow_TwoParallelStores_RunInTwoThreadsInParallel_DoNotInterfereWithEachOther()
+    public void AsyncContextNow_TwoParallelStores_RunInTwoThreadsInParallel_DoNotInterfereWithEachOther()
     {
         // 0 sec: T1 goes forward 10 minutes, asserts time, waits 2 secs
         // 1 sec: T2 asserts time, goes forward 10 minutes
@@ -65,11 +91,11 @@ public class TimecopContextStoreTests
 
             store.Mutate((ref TimecopContext o, PointInTime realNow) => o.TravelBy(TimeSpan.FromMinutes(10), realNow));
 
-            TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow.AddMinutes(10), DateTimeComparisonPrecision);
+            TimecopContextStore.AsyncContextNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow.AddMinutes(10), DateTimeComparisonPrecision);
 
             Thread.Sleep(2000);
 
-            TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow.AddMinutes(10), DateTimeComparisonPrecision);
+            TimecopContextStore.AsyncContextNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow.AddMinutes(10), DateTimeComparisonPrecision);
         });
 
         var task2 = Task.Run(() =>
@@ -78,13 +104,13 @@ public class TimecopContextStoreTests
 
             Thread.Sleep(2000);
 
-            TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow, DateTimeComparisonPrecision);
+            TimecopContextStore.AsyncContextNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow, DateTimeComparisonPrecision);
 
             store.Mutate((ref TimecopContext o, PointInTime realNow) => o.TravelBy(TimeSpan.FromMinutes(10), realNow));
         });
 
         Task.WaitAll(task1, task2);
 
-        TimecopContextStore.AsyncContextUtcNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow, DateTimeComparisonPrecision);
+        TimecopContextStore.AsyncContextNow.DateTimeUtc.Should().BeCloseTo(DateTime.UtcNow, DateTimeComparisonPrecision);
     }
 }
